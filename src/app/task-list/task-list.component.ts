f1b7cc0f6a8ca139e273b096d386fa5615e70f92
Juan Carlos Berrocal <jcberrocal@gmail.com>
2024-09-14 20:54:29 -0600

    Add & remove task

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class TaskListComponent {
   tasks = [
-    {description: 'Buy milk', completed: 'false'},
-    {description: 'Clean my room', completed: 'false'},
-    {description: 'Do excercise', completed: 'false'}
+    {description: 'Buy milk', completed: false},
+    {description: 'Clean my room', completed: false},
+    {description: 'Do excercise', completed: false}
   ]
+
+  newTask: string = '';
+
+  addTask(){
+    if(this.newTask.trim() !== ''){
+      this.tasks.push({description: this.newTask, completed: false});
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number){
+    this.tasks.splice(index, 1);
+  }
 }
